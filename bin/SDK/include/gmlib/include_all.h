c3df9e1a12aa543acc457d80f490969a929dfd94
KobeBryant114514 <116721335+KobeBryant114514@users.noreply.github.com>
2025-04-18 17:00:03 +0800

    fix: remove all

--- bin/SDK/include/gmlib/include_all.h
+++
@@ -1,9 +0,0 @@
-// This header file is automatically generated. Do not modify it manually
-// This header file is automatically generated. Do not modify it manually
-// This header file is automatically generated. Do not modify it manually
-
-#pragma once
-
-#include "include_lib.h"
-#include "include_ll.h"
-#include "include_mc.h"
