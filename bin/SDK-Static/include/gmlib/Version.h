b0d220e3a97fe445c7bfafcad2d8224e903007c1
zimuya4153 <limulu10@163.com>
2025-10-12 14:21:30 +0800

    feat: update version

--- bin/SDK-Static/include/gmlib/Version.h
+++ bin/SDK-Static/include/gmlib/Version.h
@@ -5,7 +5,7 @@
 #define GMLIB_VERSION_MINOR         5
 #define GMLIB_VERSION_PATCH         1
 /* #undef GMLIB_VERSION_PRERELEASE */
-#define GMLIB_VERSION_COMMIT_SHA    a7251fc
+#define GMLIB_VERSION_COMMIT_SHA    bb97e73
 // clang-format on
 
 #define GMLIB_VERSION_TO_STRING_INNER(ver) #ver
